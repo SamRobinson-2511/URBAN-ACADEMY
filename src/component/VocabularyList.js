461a2fa386103a787ef38c9c990319d949398ac5
Julianne Huynh <JulianneHuynh@yahoo.com>
2022-12-07 09:48:44 -0600

    commit

--- src/component/VocabularyList.js
+++ src/component/VocabularyList.js
@@ -1,16 +1,23 @@
 import React from "react";
 import VocabularyCard from "./VocabularyCard";
 
-function VocabularyList() {
-  
-
-
+function VocabularyList({terms}) {
+  const termCard = terms.map((term)=>{
+    return (
+      <VocabularyCard
+      key={term.id}
+      term={term.term}
+      definition={term.definition}
+      example={term.example}
+      moreInfo={term.moreInfo}
+      />
+    )
+  })
 
 return (
   <div>
-    {}
+    <ul className="cards">{termCard}</ul>
   </div>
-  
 );
 }
 
--- src/component/VocabularyPage.js
+++ src/component/VocabularyPage.js
@@ -9,20 +9,11 @@ import Search from "./Search"
 function VocabularyPage() {
   const [terms, setTerms]= useState([]);
 
-
-  // const options = {
-  //   method: 'GET',
-  //   headers: {
-  //     'X-RapidAPI-Key': '03af111f8cmsh681f36e41f11f5fp1691f8jsnc593d0c810f2',
-  //     'X-RapidAPI-Host': 'mashape-community-urban-dictionary.p.rapidapi.com'
-  //   }
-  // };
-  // useEffect(()=>{
-  // fetch('https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=wat', options)
-  // .then(response => response.json())
-  // .then(terms => setTerms(terms))
-  // .catch(err => console.error(err));
-  // },[])
+useEffect(()=> { 
+  fetch('http://localhost:4000/terms')
+  .then (r=> r.json())
+  .then (termArray => setTerms(termArray))
+},[])
 
  
 
